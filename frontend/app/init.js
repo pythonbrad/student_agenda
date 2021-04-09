40f0804aa5e9b663c4ec9b40d0619c9dda022e15
Fomegne Meudje <fomegnemeudje@outlook.com>
2021-04-09 19:03:15 +0100

    Fix problem with upload of file

--- frontend/app/init.js
+++ frontend/app/init.js
@@ -533,7 +533,7 @@ App = {
 			App.views.splash(
 				function () {
 					// We perform operation
-					MAX_SIZE = 1024*1024*10;
+					MAX_SIZE = 2621440;
 					App.events[0] = setInterval(function () {
 						// We verify if all the operations are finished
 						if (App.vars.can_pass) {
